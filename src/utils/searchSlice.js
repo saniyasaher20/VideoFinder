1d1d32bba8ce414da8d120bf860c56e02d16e7c2
saniyasaher20 <saniyasaher20@gmail.com>
2023-03-05 19:18:18 +0530

    added initial values for search suggestions

--- src/utils/searchSlice.js
+++ src/utils/searchSlice.js
@@ -2,7 +2,143 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const searchSlice = createSlice({
     name: "search",
-    initialState: {},
+    initialState: {
+
+        '': [],
+        j: [
+            'jethalal',
+            'jcb',
+            'johny johny yes papa',
+            'jubin nautiyal new song',
+            'jesus songs',
+            'jhume jo pathan song',
+            'jokes',
+            'jungkook',
+            'jabardasth',
+            'jannat zubair'
+        ],
+        ja: [
+            'jabardasth',
+            'jannat zubair',
+            'janam janam ka sath',
+            'jaby koay',
+            'japji sahib',
+            'jai hanuman gyan gun sagar',
+            'jana gana mana',
+            'jass manak song',
+            'jadugar',
+            'jale song'
+        ],
+        jav: [
+            'javed',
+            'java',
+            'java full course',
+            'javed akhtar',
+            'javascript',
+            'javed ali songs',
+            'javed ali',
+            'javani',
+            'javascript tutorial for beginners',
+            'javed urfi'
+        ],
+        java: [
+            'java',
+            'java full course',
+            'javascript',
+            'javani',
+            'javascript tutorial for beginners',
+            'javascript full course',
+            'javari junction',
+            'java programming',
+            'javascript tutorial',
+            'java code with harry'
+        ],
+        javas: [
+            'javascript',
+            'javascript tutorial for beginners',
+            'javascript full course',
+            'javascript tutorial',
+            'javascript mastery',
+            'javascript code with harry',
+            'javascript project',
+            'javascript interview questions and answers',
+            'javascript projects for beginners',
+            'javascript course'
+        ],
+        javasc: [
+            'javascript',
+            'javascript tutorial for beginners',
+            'javascript full course',
+            'javascript tutorial',
+            'javascript mastery',
+            'javascript code with harry',
+            'javascript project',
+            'javascript interview questions and answers',
+            'javascript projects for beginners',
+            'javascript course'
+        ],
+        javascr: [
+            'javascript',
+            'javascript tutorial for beginners',
+            'javascript full course',
+            'javascript tutorial',
+            'javascript mastery',
+            'javascript code with harry',
+            'javascript project',
+            'javascript interview questions and answers',
+            'javascript projects for beginners',
+            'javascript course'
+        ],
+        javascri: [
+            'javascript',
+            'javascript tutorial for beginners',
+            'javascript full course',
+            'javascript tutorial',
+            'javascript mastery',
+            'javascript code with harry',
+            'javascript project',
+            'javascript interview questions and answers',
+            'javascript projects for beginners',
+            'javascript course'
+        ],
+        javascrip: [
+            'javascript',
+            'javascript tutorial for beginners',
+            'javascript full course',
+            'javascript tutorial',
+            'javascript mastery',
+            'javascript code with harry',
+            'javascript project',
+            'javascript interview questions and answers',
+            'javascript projects for beginners',
+            'javascript course'
+        ],
+        javascript: [
+            'javascript',
+            'javascript tutorial for beginners',
+            'javascript full course',
+            'javascript tutorial',
+            'javascript mastery',
+            'javascript code with harry',
+            'javascript project',
+            'javascript interview questions and answers',
+            'javascript projects for beginners',
+            'javascript course'
+        ],
+        javascrit: [
+            'javascript',
+            'javascript tutorial for beginners',
+            'javascript full course',
+            'javascript tutorial',
+            'javascript mastery',
+            'javascript code with harry',
+            'javascript project',
+            'javascript interview questions and answers',
+            'javascript projects for beginners',
+            'javascript course'
+        ]
+
+    },
     reducers: {
         cacheResults: (state, action) => {
             return { ...state, ...action.payload }
